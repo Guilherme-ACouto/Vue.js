f2f5f9cb520a7050d6167b7c73d79a5f95ec6a6e
Guilherme-ACouto <couto.guilherme@pucpr.edu.br>
2024-08-03 15:51:12 -0300

    Alterando o Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -25,16 +25,39 @@ export default {
 </script>
 
 <style scoped>
+
 .header {
   display: flex;
   align-items: center;
   gap: 10px;
   position: relative; /* Define a base para o posicionamento absoluto */
+  background-color: #f0f0f0; /* Cor de fundo cinza claro */
+  padding: 10px; /* Adiciona algum espaçamento interno */
+  border-radius: 5px; /* Adiciona bordas arredondadas */
 }
 
 .filter-input {
   position: absolute;
-  top: 0;
+  top: 10;
   right: 0;
+}
+
+button {
+  background-color: #d0d0d0; /* Cor de fundo cinza para os botões */
+  border: none;
+  padding: 10px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #b0b0b0; /* Cor de fundo cinza mais escura quando o botão é hover */
+}
+
+.filter-input {
+  padding: 5px;
+  border: 1px solid #ccc; /* Borda cinza clara */
+  border-radius: 5px;
 }
 </style>
